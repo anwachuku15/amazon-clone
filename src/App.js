c1b776930cd3c01aec42dad5b5352c988014f113
anwachuku15 <anwachuku15@gmail.com>
2020-09-22 10:33:28 -0400

    wrapped app in Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
