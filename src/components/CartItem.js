4c1089cbd1ac029f5d3f10ad7dc1a8466ab7a302
anwachuku15 <anwachuku15@gmail.com>
2020-10-16 10:14:36 -0400

    added CartItem component

---
+++ src/components/CartItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CartItem.css";
+
+const CartItem = () => {
+  return (
+    <div className="cartItem">
+      <h1>Cart Item</h1>
+    </div>
+  );
+};
+
+export default CartItem;
