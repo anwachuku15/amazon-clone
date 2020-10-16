16ba9e6a5e2bee79e8b6f5dff1734a792e5507da
anwachuku15 <anwachuku15@gmail.com>
2020-10-16 10:14:09 -0400

    added Subtotal component

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -2,8 +2,39 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 
-const Subtotal = () => {
-  return <div className="subtotal"></div>;
+const Subtotal = ({ cart }) => {
+  const getCartTotal = (cart) => {
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.price;
+    });
+
+    return total;
+  };
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        value={0}
+        // value={getCartTotal(cart)}
+        renderText={(value) => (
+          <div>
+            <p>
+              Subtotal (0 items): <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift{" "}
+            </small>
+          </div>
+        )}
+        decimalScale={2}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 };
 
 export default Subtotal;
