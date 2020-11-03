696cd4919b94860635575ee8484fdb0b749b99f6
anwachuku15 <anwachuku15@gmail.com>
2020-11-03 14:04:41 -0500

    updated CartItem component

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { useStateValue } from "../../context/StateProvider";
 import "./CartItem.css";
 
-const CartItem = () => {
+const CartItem = ({ id, image, title, price, rating }) => {
+  const [{ cart }, dispatch] = useStateValue();
+
+  const removeFromCart = () => {
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      id: id,
+    });
+  };
+
   return (
     <div className="cartItem">
-      <h1>Cart Item</h1>
+      <img src={image} alt="" className="cartItem__image" />
+      <div className="cartItem__info">
+        <p className="cartItem__title">{title}</p>
+        <p className="cartItem__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="cartItem__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <span
+                key={i}
+                className="cartItem__ratingStar"
+                role="img"
+                aria-label="star"
+              >
+                🌟
+              </span>
+            ))}
+        </div>
+        <button onClick={removeFromCart}>Remove from Cart</button>
+      </div>
     </div>
   );
 };
