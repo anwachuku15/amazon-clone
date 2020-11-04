857b5f9c578403731070ee8676d5fe0a08282b16
anwachuku15 <anwachuku15@gmail.com>
2020-11-04 15:46:05 -0500

    set up API

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,19 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe");
+const stripe = require("stripe")(functions.config().stripe.secret);
+
+// API
+
+// App config
+const app = express();
+
+// Middleware
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API Routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+// Listen command
+exports.api = functions.https.onRequest(app);
