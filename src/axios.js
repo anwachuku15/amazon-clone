3d97f2bc84ac9c67bf3e5aeccf4946ff2f801b46
anwachuku15 <anwachuku15@gmail.com>
2020-11-04 20:24:57 -0500

    changed baseURL for production

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-7b53f/us-central1/api",
+  // baseURL: "http://localhost:5001/clone-7b53f/us-central1/api",
+  baseURL: "https://us-central1-clone-7b53f.cloudfunctions.net/api",
 });
 
 export default instance;
